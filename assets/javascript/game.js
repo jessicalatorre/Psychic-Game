4e1ae1157a55fc076a3667f06363e05a8f004c74
Jessica La Torre <jessicalatorre@Jessicas-MacBook-Pro.local>
2018-03-11 18:13:45 -0500

    rearranged and renamed variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,31 +7,19 @@
 //If player wins, populate "wins" sections with number of wins
 
 //Use loop to move on to new game?
-// var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerChoices = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"]
-var guessesRemaining = 9;
+// var computerChoices = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"]
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var playerWins = 0
 var playerLoses = 0
-var userGuess = ""; //This is letter the user will press on keyboard ; must declare this - input as blank - line 27 will populate with a letter once the user keys up (chooses)
-var letterToGuess = ""; //computer will choose word; player will try to uncover. computer will populate on line 20
-var lettersInArray =[]; //this is an open array as the word will be randomly selected from the var options arry on line 10 - horse, pig, etc
-// var wordChosen = randomWord.split(" ");
-// wordChosen = ["p", "i", "g"];
-
-letterToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]; //cpu randomly choosing elements based on total options in array
-console.log(letterToGuess);
-lettersInArray = letterToGuess.split("");
-console.log(lettersInArray); //once a word is chosen
-guessesRemaining = document.getElementById("guessesRemaining"); 
-//don't use inner html we're not grabbing anything from the html, 
-//we're grabbing a variable from the separate JS file!!
+var guessesRemaining = 9;
+var userGuessSoFar = ""; //This is letter the user will press on keyboard ; must declare this - input as blank - line 27 will populate with a letter once the user keys up (chooses)
 
 document.onkeyup = function() {
-    // alert("You Pressed a Key!");
-    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(userGuess);
-    guessesRemaining.textContent = userGuess;
+    var userGuesses = String.fromCharCode(eventkeyCode).toLowerCase();
+    var lettertoGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+    userGuessSoFar.push(userGuesses)
 
+if (userGuesses === c)
 }
 
 
