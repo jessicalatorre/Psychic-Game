ff9f33463fdf21987a081e28b1efa8ad04be201e
Jessica La Torre <jessicalatorre@Jessicas-MacBook-Pro.local>
2018-03-12 18:28:23 -0500

    reviewed variables names, and ensured all of my functions was included in curly braces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,55 +12,44 @@ var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 var playerWins = 0;
 var playerLosses = 0;
 var guessesRemaining = 9;
-// var userGuess = []; //This is letter the user will press on keyboard ; must declare this - input as blank - line 27 will populate with a letter once the user keys up (chooses)
+var AllUserGuessesArray = []; //This is letter the user will press on keyboard ; must declare this - input as blank - line 27 will populate with a letter once the user keys up (chooses)
 
 // function reset(){ //Can try using this rather than resetting in under conditional statements, try creating a fucntion and inputting the function under each if statement. 
 //     totalGuesses = 9;
 //     guessesRemaining = 9;
-//     userGuess = [];
-    // updateLettersToGuess();  // not sure what to do with these 3 functions
-    // GuessesLeft();
-    // GuessessoFar();
+//     userGuess = "";
+//     updateLettersToGuess();  // not sure what to do with these 3 functions
+//     GuessesLeft();
+//     GuessessoFar();
 // };
 
-// var updateLetterToGuess = function() {    
-//     // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)       
-    // computerGuess = letterArray[Math.floor(Math.random() * letterArray.length)];  
-//};
-
-document.onkeyup = function() {
-    var userGuessesSoFar = String.fromCharCode(event.keyCode).
-    toLowerCase(); // user types in choice & it's stored in var user Guesses
-    console.log(userGuessesSoFar);
-}
+document.onkeyup = function(event) {
+    var userInputGuess =event.key;
+    // var userGuess = String.fromCharCode(event.keyCode).
+    // toLowerCase(); // user types in choice & it's stored in var user Guesses
+    console.log(userInputGuess);
+    AllUserGuessesArray.push(userInputGuess) //this should take push value from AllUserGuessesArray into userInputGuess
     computerGuess = letterArray[Math.floor(Math.random() * letterArray.length)];  
-    //var computerGuess = letterArray[Math.floor(Math.random()*letterArray.length)]; //CPU randomly chooses a letter
-    userGuessSoFar.push(userGuess) //this should take value stored value in userGuessSoFar and push to userGueses
-    // updateLettersToGuess();
+    
+    if (userInputGuess === computerGuess) { //condition for winning. comparing userGuesses to computerGuess. If they're the same...
+        playerWins++; 
+        // reset(); 
+        guessesRemaining = 9;     
+        AllUserGuessesArray.length = [];     
+    }
+    else if (userInputGuess !== computerGuess) {
+        playerLosses==1;
+        guessesRemaining = 9;
+    }
+    var html = "<h1>The Psychic Game </h1>" + 
+    "<p>Guess what letter I'm thinking of</p>" +
+    "<p>Guesses so far: " +  AllUserGuessesArray +"</p>"
+    "<p>Wins: " + playerWins + "</p>" +
+    "<p>Losses: " + playerLosses + "</p>" +
+    "<p>Guesses Remaining" + guessesRemaining + "</p>"
 
-if (userGuess === computerGuess) { //condition for winning. comparing userGuesses to computerGuess. If they're the same...
-    playerWins++; 
-    // reset(); 
-    guessesRemaining = 9;     
-    guessesSoFar.length = 0;     
+    document.querySelector("#psychicGame").innerHTML = html;
 }
-else if (guessesRemaining == 0) { 
-    playerLosses++1;
-    guessesRemaining = 0;
-    userGuessSoFar.length = 0;     
-}
-else if (userGuess !== computerGuess) {
-    guessesRemaining--;
-}
-var html = "<h1>The Psychic Game </h1>" + 
-"<p>Guess what letter I'm thinking of</p>" +
-"<p>Guesses so far: " +  GuessessoFar +"</p>"
-"<p>Wins: " + playerWins + "</p>" + +
-"<p>Losses: " + playerLosses + "</p>" +
-"<p>Guesses Remaining" + guessesRemaining + "</p>"
-
-
-document.querySelector("#psychicGame").innerHTML = html;
 
 // document.querySelector("#guessessleft").innerHTML = guessesLeft; use this if you use functions in your if and else if statements
 
