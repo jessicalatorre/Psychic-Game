32cdfc4a8a7ec306310a3a66082bb25493403f00
Jessica La Torre <jessicalatorre@Jessicas-MacBook-Pro.local>
2018-03-12 00:07:39 -0500

    added some css styling. Tested onkeyup event for user Guesses so far variable by logging to console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,56 +7,61 @@
 //If player wins, populate "wins" sections with number of wins
 
 //Use loop to move on to new game?
-// var computerChoices = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"]
+
 var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var playerWins = 0
-var playerLosses = 0
+var playerWins = 0;
+var playerLosses = 0;
 var guessesRemaining = 9;
-var userGuesses = []; //This is letter the user will press on keyboard ; must declare this - input as blank - line 27 will populate with a letter once the user keys up (chooses)
+// var userGuess = []; //This is letter the user will press on keyboard ; must declare this - input as blank - line 27 will populate with a letter once the user keys up (chooses)
 
-function reset(){ //rather than resetting in under conditional statements, try creating a fucntion and inputting the function under each if statement
-    totalGuesses = 9;
-    guessesLeft = 9;
-    guessedLetters = [];
-    updateLettersToGuess();
-    GuessesLeft();
-    GuessessoFar();
-};
+// function reset(){ //Can try using this rather than resetting in under conditional statements, try creating a fucntion and inputting the function under each if statement. 
+//     totalGuesses = 9;
+//     guessesRemaining = 9;
+//     userGuess = [];
+    // updateLettersToGuess();  // not sure what to do with these 3 functions
+    // GuessesLeft();
+    // GuessessoFar();
+// };
 
-var updateLetterToGuess = function() {    
-    // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)       
-    computerGuess = letterArray[Math.floor(Math.random() * letterArray.length)];  
-};
+// var updateLetterToGuess = function() {    
+//     // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)       
+    // computerGuess = letterArray[Math.floor(Math.random() * letterArray.length)];  
+//};
 
 document.onkeyup = function() {
-    var userGuessesSoFar = String.fromCharCode(eventkeyCode).toLowerCase(); // user types in choice & it's stored in var user Guesses
-    //var computerGuesses = letterArray[Math.floor(Math.random()*letterArray.length)]; //CPU randomly chooses a letter
-    updateLettersToGuess();
-    userGuessSoFar.push(userGuesses) //this should take value stored value in userGuessSoFar and push to userGueses
+    var userGuessesSoFar = String.fromCharCode(event.keyCode).
+    toLowerCase(); // user types in choice & it's stored in var user Guesses
+    console.log(userGuessesSoFar);
+}
+    computerGuess = letterArray[Math.floor(Math.random() * letterArray.length)];  
+    //var computerGuess = letterArray[Math.floor(Math.random()*letterArray.length)]; //CPU randomly chooses a letter
+    userGuessSoFar.push(userGuess) //this should take value stored value in userGuessSoFar and push to userGueses
+    // updateLettersToGuess();
 
-if (userGuesses === computerGuesses) { //condition for winning. comparing userGuesses to computerGuess. If they're the same...
+if (userGuess === computerGuess) { //condition for winning. comparing userGuesses to computerGuess. If they're the same...
     playerWins++; 
-    reset(); 
-    updateLettersToGuess();              
+    // reset(); 
+    guessesRemaining = 9;     
+    guessesSoFar.length = 0;     
 }
-
-else if (userGuesses !=== computerGuesses) {  
-    playerLosses++;
-    // guessesRemaining = 9;
-    reset();   
-    updateLettersToGuess();
+else if (guessesRemaining == 0) { 
+    playerLosses++1;
+    guessesRemaining = 0;
+    userGuessSoFar.length = 0;     
 }
-
+else if (userGuess !== computerGuess) {
+    guessesRemaining--;
+}
 var html = "<h1>The Psychic Game </h1>" + 
 "<p>Guess what letter I'm thinking of</p>" +
-"<p>Wins: " + playerWins + "</p>" +
+"<p>Guesses so far: " +  GuessessoFar +"</p>"
+"<p>Wins: " + playerWins + "</p>" + +
 "<p>Losses: " + playerLosses + "</p>" +
-"<p>Guesses Left: " + GuessesLeft + "</p>" +
-"<p>Guesses So Far: " +  GuessessoFar +"</p>";
+"<p>Guesses Remaining" + guessesRemaining + "</p>"
 
 
-document.querySelector("#psychicGame") = html;
+document.querySelector("#psychicGame").innerHTML = html;
 
-// document.querySelector("#guessessleft").innerHTML = guessesLeft;
+// document.querySelector("#guessessleft").innerHTML = guessesLeft; use this if you use functions in your if and else if statements
 
 
