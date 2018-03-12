82cbb949de9db05143f37a8462f8241998f5ff7a
Jessica La Torre <jessicalatorre@Jessicas-MacBook-Pro.local>
2018-03-11 19:44:20 -0500

    Added reset function, so wins, losses, and guesses will rest after each game. Will put function at the end of each if statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,19 +8,44 @@
 
 //Use loop to move on to new game?
 // var computerChoices = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"]
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var playerWins = 0
-var playerLoses = 0
+var playerLosses = 0
 var guessesRemaining = 9;
-var userGuessSoFar = ""; //This is letter the user will press on keyboard ; must declare this - input as blank - line 27 will populate with a letter once the user keys up (chooses)
+var userGuesses = []; //This is letter the user will press on keyboard ; must declare this - input as blank - line 27 will populate with a letter once the user keys up (chooses)
+
+function reset(){ //rather than resetting in under conditional statements, try creating a fucntion and inputting the function under each if statement
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
+    updateLettersToGuess();
+    updateGuessesLeft();
+    updateGuessessoFar();
+};
+
+var updateLetterToGuess = function() {    
+    // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)       
+    computerGuess = letterArray[Math.floor(Math.random() * letterArray.length)];  
+};
 
 document.onkeyup = function() {
-    var userGuesses = String.fromCharCode(eventkeyCode).toLowerCase();
-    var lettertoGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-    userGuessSoFar.push(userGuesses)
+    var userGuessesSoFar = String.fromCharCode(eventkeyCode).toLowerCase(); // user types in choice & it's stored in var user Guesses
+    //var computerGuesses = letterArray[Math.floor(Math.random()*letterArray.length)]; //CPU randomly chooses a letter
+    updateLettersToGuess();
+    userGuessSoFar.push(userGuesses) //this should take value stored value in userGuessSoFar and push to userGueses
 
-if (userGuesses === c)
+if (userGuesses === computerGuesses) { //condition for winning. comparing userGuesses to computerGuess. If they're the same...
+    playerWins++; 
+    reset();                  //Then one point is added to var player wins
+    // guessesRemaining = 9;           //This should reset the guess counter back to 9
+    // guessesSoFar.lenth = 0;         // This shiould reset the gueses so far back to 0
+}
+
+else if (userGuesses !=== computerGuesses) {  
+    playerLosses++
+    // guessesRemaining = 9;
+    // guesse
 }
 
 
-    
+
